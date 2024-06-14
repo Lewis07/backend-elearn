a9c3e3117809a102540afbb27cad89f68095554e
RLewis <rabarison.lewis@yahoo.com>
2024-06-14 21:08:21 +0300

    mise en place prefixe global /api et recuperation en string des erreurs de champs

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,14 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 4000;
+  app.setGlobalPrefix("/api");
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[] = []) => {
         return new BadRequestException(
           validationErrors.map((error) => ({
             field: error.property,
-            error: Object.values(error.constraints)
+            error: Object.values(error.constraints)[0]
           }))
         );
       },
