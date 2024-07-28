076f03b489c970bce45a8c5fc80d8029b6c418fe
RLewis <rabarison.lewis@yahoo.com>
2024-07-28 20:58:12 +0300

    modif reponse api mise a jour profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,24 @@
 import { Body, Controller, Patch, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { UpdateProfileDto } from './dto/update-profile.dto';
-import { Response } from 'express';
 import { UsersService } from './users.service';
 
 @Controller('')
 export class UsersController {
-    constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
-    @UseGuards(AuthGuard)
-    @Patch("update-profile")
-    async updateProfile(@Req() req: any, @Res() res: Response, @Body() updateProfileDto: UpdateProfileDto) {
-        await this.usersService.updateProfile(req.user.id, updateProfileDto);
+  @UseGuards(AuthGuard)
+  @Patch('update-profile')
+  async updateProfile(
+    @Req() req: any,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    const user = await this.usersService.updateProfile(
+      req.user.id,
+      updateProfileDto,
+    );
+    const { usr_username, usr_email, usr_firstname, usr_lastname } = user;
 
-        return res.status(200).json({ status: "success" });
-    }
+    return { usr_username, usr_email, usr_firstname, usr_lastname };
+  }
 }
