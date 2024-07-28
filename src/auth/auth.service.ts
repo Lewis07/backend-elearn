b4f6a2060907f0a73dc97323a2a5ef33ac02541b
RLewis <rabarison.lewis@yahoo.com>
2024-07-28 18:05:30 +0300

    modif retour payload jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, ClassSerializerInterceptor, Injectable, UnauthorizedException, UseInterceptors } from '@nestjs/common';
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  Injectable,
+  UnauthorizedException,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { validatePassword } from '../utils/validatePassword.utils';
@@ -19,17 +25,29 @@ export class AuthService {
     private jwtService: JwtService,
     @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(UserReset.name) private userResetModel: Model<UserReset>,
-    private stripeCustomerService: StripeCustomerService
+    private stripeCustomerService: StripeCustomerService,
   ) {}
 
   async singIn(signInDto: SignInDto): Promise<{ accessToken: string }> {
     const user = await this.usersService.findOneByEmail(signInDto.usr_email);
 
-    if (!user || !(await validatePassword(signInDto.usr_password, user?.usr_password))) {
-      throw new UnauthorizedException("Invalid credentials");
+    if (
+      !user ||
+      !(await validatePassword(signInDto.usr_password, user?.usr_password))
+    ) {
+      throw new UnauthorizedException('Invalid credentials');
     }
 
-    const payload = { id: user.id, username: user.usr_username, email: user.usr_email };
+    const payload = {
+      id: user.id,
+      username: user.usr_username,
+      email: user.usr_email,
+      firstname: user.usr_firstname,
+      lastname: user.usr_lastname,
+      photo: user.usr_photo,
+      type: user.usr_type
+    };
+    
     const accessToken = await this.jwtService.signAsync(payload);
 
     return { accessToken };
@@ -38,20 +56,28 @@ export class AuthService {
   @UseInterceptors(ClassSerializerInterceptor)
   async signUp(registerDto: RegisterDto): Promise<User> {
     const hash_password = await hashPassword(registerDto.usr_password);
-    const data = {...registerDto, usr_password: hash_password};
+    const data = { ...registerDto, usr_password: hash_password };
     const user = await this.userModel.create(data);
     let customerId = null;
 
     try {
-      const stripeCustomer = await this.stripeCustomerService.createcustomer(user.usr_username, user.usr_email);
+      const stripeCustomer = await this.stripeCustomerService.createcustomer(
+        user.usr_username,
+        user.usr_email,
+      );
       customerId = stripeCustomer.customer_id;
     } catch (error) {
       console.error(error);
     }
 
-    const userWithCustomerIdStripe = await this.userModel.findByIdAndUpdate(user._id, { stripe_customer_id: customerId });
+    const userWithCustomerIdStripe = await this.userModel.findByIdAndUpdate(
+      user._id,
+      { stripe_customer_id: customerId },
+    );
 
-    return plainToClass(User, userWithCustomerIdStripe, { excludeExtraneousValues: true});
+    return plainToClass(User, userWithCustomerIdStripe, {
+      excludeExtraneousValues: true,
+    });
   }
 
   async resetPassword(email: string, token: string): Promise<UserReset> {
@@ -61,7 +87,7 @@ export class AuthService {
     const data = {
       usr_rest_email: email,
       usr_rest_token: token,
-      usr_rest_expired_at: tokenExpiredAt
+      usr_rest_expired_at: tokenExpiredAt,
     };
 
     return await this.userResetModel.create(data);
