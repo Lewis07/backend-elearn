46baadc70309f80c80abeaf84d8658196d89e613
RLewis <rabarison.lewis@yahoo.com>
2024-12-12 07:52:36 +0300

    installation et mise en place de documentation swagger

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,38 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ValidationError, useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 4000;
-  app.setGlobalPrefix("/api");
+
+  app.setGlobalPrefix('/api');
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[] = []) => {
         return new BadRequestException(
           validationErrors.map((error) => ({
             field: error.property,
-            error: Object.values(error.constraints)[0]
-          }))
+            error: Object.values(error.constraints)[0],
+          })),
         );
       },
-      stopAtFirstError: true
-    })
+      stopAtFirstError: true,
+    }),
   );
-  useContainer(app.select(AppModule), { fallbackOnErrors: true })
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(port);
 }
 
