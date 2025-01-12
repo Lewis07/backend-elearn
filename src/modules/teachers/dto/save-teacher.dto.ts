92489acdf8451e55e79429daf97e78193c91c2bd
RLewis <rabarison.lewis@yahoo.com>
2025-01-12 09:41:41 +0300

    doc swagger endpoint enseignant

--- src/modules/teachers/dto/save-teacher.dto.ts
+++ src/modules/teachers/dto/save-teacher.dto.ts
@@ -1,11 +1,19 @@
-import { IsNotEmpty, IsOptional } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class SaveTeacherDto {
-    @IsOptional()
-    @IsNotEmpty({ message: "Biography is required" })
-    tchr_biography: string;
+  @ApiProperty({
+    example:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elit sagittis, at sollicitudin odio tristique fusce suscipit pretium vehicula, velit duis cubilia maecenas',
+    description: "Teacher's biography",
+  })
+  @IsNotEmpty({ message: 'Biography is required' })
+  tchr_biography: string;
 
-    @IsOptional()
-    @IsNotEmpty({ message: "Domain is required" })
-    tchr_domain: string;
+  @ApiProperty({
+    example: 'Software ingeneer',
+    description: "Teacher's domain",
+  })
+  @IsNotEmpty({ message: 'Domain is required' })
+  tchr_domain: string;
 }
--- src/modules/teachers/teachers.controller.ts
+++ src/modules/teachers/teachers.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -14,31 +16,60 @@ import { TeachersService } from './teachers.service';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { SaveTeacherDto } from './dto/save-teacher.dto';
 import { Response } from 'express';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 
 @Controller('teachers')
 export class TeachersController {
   constructor(private teachersService: TeachersService) {}
 
-  @UseGuards(AuthGuard)
-  @Get('list')
+  @Get()
+  @ApiOkResponse({
+    description: 'The teachers have been successfully retrieved.',
+  })
   async list() {
     return await this.teachersService.findAll();
   }
 
-  @UseGuards(AuthGuard)
-  @Get('show/:id')
+  @Get(':id')
+  @ApiOkResponse({
+    description: 'The teacher have been successfully retrieved.',
+  })
+  @ApiNotFoundResponse({
+    description: 'The teacher is not found.',
+  })
   async show(@Param('id') id: string) {
     return await this.teachersService.findById(id);
   }
 
+  @Post()
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
-  @Post('add')
+  @ApiCreatedResponse({
+    description: 'The teacher has been successfully created.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request.',
+  })
   async add(@Req() req: any, @Body() saveTeacherDto: SaveTeacherDto) {
     return await this.teachersService.store(req.user.id, saveTeacherDto);
   }
 
+  @Patch(':id')
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
-  @Patch('update/:id')
+  @ApiOkResponse({
+    description: 'The teacher has been successfully updated.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request.',
+  })
   async update(
     @Param('id') id: string,
     @Body() saveTeacherDto: SaveTeacherDto,
@@ -46,11 +77,14 @@ export class TeachersController {
     return await this.teachersService.update(id, saveTeacherDto);
   }
 
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
-  @Delete('delete/:id')
-  async delete(@Param('id') id: string, @Res() res: Response) {
+  @ApiNoContentResponse({
+    description: 'The teacher has been successfully deleted.',
+  })
+  async delete(@Param('id') id: string) {
     await this.teachersService.delete(id);
-
-    return res.json({ teacherId: id });
   }
 }
