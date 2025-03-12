2f844dd85f299a91d05bb35ec786ced91f58ac18
RLewis <rabarison.lewis@yahoo.com>
2025-03-12 23:09:47 +0300

    ajout d'un identifiant sur le mailer module

--- src/shared/mailer/send-mail.module.ts
+++ src/shared/mailer/send-mail.module.ts
@@ -8,6 +8,11 @@ import { MailerModule } from '@nestjs-modules/mailer';
         transport: {
           host: process.env.EMAIL_HOST,
           port: Number(process.env.EMAIL_PORT),
+          secure: false,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+          },
         },
         defaults: {
           from: process.env.EMAIL_FROM,
